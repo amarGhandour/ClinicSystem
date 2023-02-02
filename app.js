ee15a870d1444dee42091f41baf778d1b4b0aca4
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-02-02 20:50:54 +0200

    medicine route added

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 const express = require("express");
-const morgan = require('morgan')
-const authMW = require("./middlewares/authMW")
+const morgan = require("morgan");
+const authMW = require("./middlewares/authMW");
 
-require('dotenv').config({path: './config/.env'});
-const connectDB = require('./config/db');
-const errorHandler = require('./middlewares/error');
+require("dotenv").config({ path: "./config/.env" });
+const connectDB = require("./config/db");
+const errorHandler = require("./middlewares/error");
 const ErrorResponse = require("./utils/ErrorResponse");
 
 const clinicServicesRoute = require("./routes/clinicServicesRoute");
 const patientRoute = require("./routes/patientsRoute");
 const authRoute = require("./routes/authRoute");
+const medicineRoute = require("./routes/medicineRoute");
 
 const server = express();
 
 let port = process.env.PORT || 8080;
-connectDB().then(res => {
+connectDB()
+  .then((res) => {
     server.listen(port, () => {
-        console.log("I am listening........", port);
+      console.log("I am listening........", port);
     });
-}).catch(err => console.log(`error: ${err.message}`));
+  })
+  .catch((err) => console.log(`error: ${err.message}`));
 
 // logger
-server.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+server.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms")
+);
 server.use(express.json());
 
 // routes
+server.use("/api/v1/medicines", medicineRoute);
 server.use("/api/v1/auth", authRoute);
 
 server.use(authMW);
@@ -32,7 +38,7 @@ server.use("/api/v1/services", clinicServicesRoute);
 server.use("/api/v1/patients", patientRoute);
 
 server.use((request, response, next) => {
-    next(new ErrorResponse("Not found", 404));
+  next(new ErrorResponse("Not found", 404));
 });
 
 server.use(errorHandler);
