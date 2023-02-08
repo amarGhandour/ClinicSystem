51aa48c4459d4f5d190d14a811d681851550f9e0
AfnanElhussini <afnanrlhussini23@gmail.com>
2023-02-08 23:17:47 +0200

    Just Formatting

--- controllers/clinicController.js
+++ controllers/clinicController.js
@@ -2,29 +2,11 @@ const ClinicServices = require("../models/clinic");
 const ErrorResponse = require("../utils/ErrorResponse");
 
 exports.getClinicServices = (req, res, next) => {
-  //build query
-  // const queryObject = { ...req.query };
-  // const excludedFields = ["page", "sort", "limit", "fields"];
-  // excludedFields.forEach((el) => delete queryObject[el]);
-  // //build the query string
-  // let queryStr = JSON.stringify(queryObject);
-  // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-  // let sortBy = req.query.sort
-  //   ? req.query.sort.split(",").join(" ")
-  //   : "-createdAt";
-  // let limitFields = req.query.fields
-  //   ? req.query.fields.split(",").join(" ")
-  //   : "-__v";
-  // let page = parseInt(req.query.page) || 1;
-  // let limit = parseInt(req.query.limit) || 100;
-  // let skip = (page - 1) * limit;
-  // // Network request
   let clinicCount = ClinicServices.countDocuments().then((count) => {
     return count;
   });
-  if (req.queryBuilder.skip >= clinicCount) throw new Error("This page does not exist");
-  //Execute the query
+  if (req.queryBuilder.skip >= clinicCount)
+    throw new Error("This page does not exist");
 
   //let query =
   ClinicServices.find(JSON.parse(req.queryBuilder.queryStr))
@@ -47,35 +29,35 @@ exports.getClinicServices = (req, res, next) => {
 
 exports.addClinicServices = (req, res, next) => {
   console.log(req.body);
-    const clinicServices = new ClinicServices({
-      name: req.body.name,
-      location: req.body.location,
-      phone: req.body.phone,
-      email: req.body.email,
-      description: req.body.description,
-      services: req.body.services,
-    });
-    clinicServices
-      .save()
-      .then((result) => {
-        return res.status(201).json({
-          success: true,
-          data: result,
-        });
-      })
-      .catch((err) => {
-        next(new ErrorResponse(err.message, 500));
+  const clinicServices = new ClinicServices({
+    name: req.body.name,
+    location: req.body.location,
+    phone: req.body.phone,
+    email: req.body.email,
+    description: req.body.description,
+    services: req.body.services,
+  });
+  clinicServices
+    .save()
+    .then((result) => {
+      return res.status(201).json({
+        success: true,
+        data: result,
       });
+    })
+    .catch((err) => {
+      next(new ErrorResponse(err.message, 500));
+    });
 };
 
 exports.updateClinicServices = (req, res, next) => {
-  
-    const clinicServices = new ClinicServices();
-    clinicServices
-      .updateOne({ _id: req.params.id },
-        
-        {
-          $set: {
+  const clinicServices = new ClinicServices();
+  clinicServices
+    .updateOne(
+      { _id: req.params.id },
+
+      {
+        $set: {
           name: req.body.name,
           location: req.body.location,
           phone: req.body.phone,
@@ -83,19 +65,18 @@ exports.updateClinicServices = (req, res, next) => {
           website: req.body.website,
           description: req.body.description,
           services: req.body.services,
-        }}
-        
+        },
+      }
     )
-      .then((result) => {
-        res.status(200).json({
-          success: true,
-          data: result,
-        });
-      })
-      .catch((err) => {
-        next(new ErrorResponse(err.message, 500));
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        data: result,
       });
- 
+    })
+    .catch((err) => {
+      next(new ErrorResponse(err.message, 500));
+    });
 };
 
 exports.deleteClincServices = (req, res, next) => {
