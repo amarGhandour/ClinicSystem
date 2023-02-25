5284294417a403030db101315ec3360accb0c891
Mahmoud Mohamed Ali ALi <99610976+iMahmouddAli@users.noreply.github.com>
2023-02-25 23:06:56 +0200

    aa

--- routes/prescriptionRoute.js
+++ routes/prescriptionRoute.js
@@ -1,22 +1,61 @@
 const express = require("express");
-const {body, query, param} = require("express-validator");
-const {prescriptionValidation, idValidation, prescriptionValidationForPatch} = require("../middlewares/dataValidator");
+const { body, query, param } = require("express-validator");
+const {
+  prescriptionValidation,
+  idValidation,
+  prescriptionValidationForPatch,
+} = require("../middlewares/dataValidator");
 const validator = require("../middlewares/errorValidator");
 const controller = require("../Controllers/prescriptionController");
-const {authorize} = require("../middlewares/authMW");
+const { authorize } = require("../middlewares/authMW");
 const router = express.Router();
 
-
-router.route("/")
-    .get(authorize('admin'), controller.getAllPrescriptions)
-    .post(authorize('admin', 'doctor'), prescriptionValidation, validator, controller.addPrescription)
-    .patch(authorize('admin', 'doctor'), prescriptionValidationForPatch, validator, controller.updatePrescription);
+router
+  .route("/")
+  .get(authorize("admin"), controller.getAllPrescriptions)
+  .post(
+    authorize("admin", "doctor"),
+    prescriptionValidation,
+    validator,
+    controller.addPrescription
+  )
+  .patch(
+    authorize("admin", "doctor"),
+    prescriptionValidationForPatch,
+    validator,
+    controller.updatePrescription
+  );
 
 router
-    .get("/doctor/:id", authorize('admin', 'doctor'), idValidation, validator, controller.getAllPrescriptionsForDoctor)
-    .get("/patient/:id", idValidation, validator, controller.getAllPrescriptionsForPatient)
+  .get(
+    "/doctor/:doctorId",
+    authorize("admin", "doctor"),
+    idValidation,
+    validator,
+    controller.getAllPrescriptionsForDoctor
+  )
+  .get(
+    "/patient/:patientId",
+    idValidation,
+    validator,
+    controller.getAllPrescriptionsForPatient
+  );
 
-router.patch("/:id/addDrug", authorize('admin', 'doctor'), idValidation, prescriptionValidationForPatch, validator, controller.addDrugToPrescription);
-router.patch("/:id/removeDrug", authorize('admin', 'doctor'), idValidation, prescriptionValidationForPatch, validator, controller.removeDrugFromPrescription)
+router.patch(
+  "/:id/addDrug",
+  authorize("admin", "doctor"),
+  idValidation,
+  prescriptionValidationForPatch,
+  validator,
+  controller.addDrugToPrescription
+);
+router.patch(
+  "/:id/removeDrug",
+  authorize("admin", "doctor"),
+  idValidation,
+  prescriptionValidationForPatch,
+  validator,
+  controller.removeDrugFromPrescription
+);
 
 module.exports = router;
